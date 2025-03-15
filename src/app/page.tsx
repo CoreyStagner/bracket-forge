85f49b6aec83179685985d1fec443b751b00c1fb
CoreyStagner <cresc0428@gmail.com>
2025-03-15 08:01:08 -0400

    cleaned up unused filed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { SignInButton } from "../components/signin-button";
 
 const Home = () => {
   return (
     <div className="home-page">
       <h1>Home</h1>
-      <SignInButton className="signin-button" />
     </div>
   );
 };
